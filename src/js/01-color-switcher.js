8d774fe836d121aed38ec58f518c659f8cd687b3
Pavel Lobtsov <plobtsov@gmail.com>
2022-04-06 16:02:53 +0300

    task 1 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const buttonStartEl = document.querySelector('[data-start]');
+const buttonStopEl = document.querySelector('[data-stop]');
+const bodyEL = document.querySelector('body');
 
+let timerId = null;
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    bodyEL.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStartEl.disabled = true;
+  buttonStopEl.disabled = false;
+}
+
+buttonStartEl.addEventListener('click', onStartBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  buttonStartEl.disabled = false;
+  buttonStopEl.disabled = true;
+}
+buttonStopEl.addEventListener('click', onStopBtnClick);
